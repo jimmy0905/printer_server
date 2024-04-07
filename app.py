933fa0742b4ea5a635c0316642c0f6d569d84510
jimmy_desktop <fktse10@gmail.com>
2024-04-07 20:05:31 +0800

    log message added

--- app.py
+++ app.py
@@ -237,8 +237,10 @@ def print_badge(
     c.drawString(margin, margin, bottom_left_text)
     c.drawRightString(width - margin, margin, bottom_right_text)
     # draw the qr code
+    print("fullName:" + fullName)
+    print("qrcode:" + qrcode_id)
     if qrcode_id is not None and qrcode_id != "":
-        print(qrcode_id)
+        
         size = 20 * mm
         qrcode_margin = 5 * mm
         qr = QRCodeImage(qrcode_id, size=size)
